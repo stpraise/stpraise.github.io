d1395028f54a583511f38830ec2bc1a3a9886fcf
stpraise <139150142+stpraise@users.noreply.github.com>
2023-08-07 17:16:51 +0300

    Add files via upload

---
+++ shyheadphones.js
@@ -0,0 +1,21 @@
+var ProductImage = document.getElementById("ProductImage");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function () {
+    ProductImage.src = SmallImg[0].src;
+}
+SmallImg[1].onclick = function () {
+    ProductImage.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function () {
+    ProductImage.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function () {
+    ProductImage.src = SmallImg[3].src;
+}
+SmallImg[4].onclick = function () {
+    ProductImage.src = SmallImg[4].src;
+}
+SmallImg[5].onclick = function () {
+    ProductImage.src = SmallImg[5].src;
+}
